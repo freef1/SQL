8b99df0c8947171f44df6a84d98d82401318a067
freef1 <lebronvince07@gmail.com>
2021-11-03 17:19:36 +0300

    russian text

--- HW_09/Script_HW_9.sql
+++ HW_09/Script_HW_9.sql
@@ -1,10 +1,10 @@
-create table employees (
+Ôªøcreate table employees (
 	id serial primary key,
 	employee_name varchar (50) not null
 );
 
 insert into employees (id, employee_name)
-values (default, '›ÎÎ‡');
+values (default, '–≠–ª–ª–∞');
 
 select * from employees;
 
